292ece441588bba1fc53fd4ca5eded97a2f19b6d
amstr1k <sferryf@gmail.com>
2020-03-04 00:46:54 +0300

    init commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param} from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all users';
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: number): string {
+    return `This action returns User Id ${id}`;
+  }
+}
