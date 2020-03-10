9b33d7d4f294b2ebcfd1fadb93e13caa908f7efe
amstr1k <sferryf@gmail.com>
2020-03-10 23:43:54 +0300

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,5 @@ module.exports = {
   env: {
     node: true,
     jest: true,
-  },
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+  }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { AppStart } from './app-start.module';
 import { NestFactory } from '@nestjs/core';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppStart);
   await app.listen(3000);
 }
